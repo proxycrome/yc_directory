18f31d718b36f73a4191f9174b82b85d135fab35
proxycrome <e.v.ezeonwuka@gmail.com>
2025-06-25 22:45:31 +0100

    initial commit

---
+++ components/View.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Ping from "./Ping";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { after } from "next/server";
+
+const View = async ({ id }: { id: string }) => {
+  const { views: totalViews } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  // Increment the view count after the component is rendered
+  // This is to ensure that the view count is updated only once per page load
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
+
+  return (
+    <div className="view-container">
+      <div className="absolute -top-2 -right-2">
+        <Ping />
+      </div>
+      <p className="view-text">
+        <span className="font-black">
+          {totalViews} {Number(totalViews) === 1 ? "view" : "views"}
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default View;
